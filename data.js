3e50193dff71f5338d8edc45e4044748376a21ea
Kaan <74866163+KaanOzen-mF@users.noreply.github.com>
2023-05-15 11:42:45 +0300

    Data file updated

--- data.js
+++ data.js
@@ -1,13 +1,43 @@
 export default [
     {
         id: 1,
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        title: "Neuschwanstein Castle",
+        location: "Germany",
+        googleMapsUrl: "https://goo.gl/maps/nCS4CbXapsZxKEMH6",
         startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        endDate: "14 Jan, 2021",
+        description: "Built atop a rock ledge over the Pöllat Gorge in the Bavarian Alps by order of Bavaria’s King Louis II (“Mad King Ludwig”). Construction began in 1868 and was never completed.",
+        imageUrl: "https://unsplash.com/photos/19T-waevMzE"
     },
+    {
+        id: 2,
+        title: "Black Forest",
+        location: "Germany",
+        googleMapsUrl: "https://goo.gl/maps/GivBwxafb3J9Ajqs5",
+        startDate: "15 Jan, 2021",
+        endDate: "18 Jan, 2021",
+        description: "It is mainly a granite highland with rounded summits, although its northern part comprises forested sandstone, and it is bordered to the south by a narrow band of lower and more fertile limestone.",
+        imageUrl: "https://unsplash.com/photos/oQ5O2ExIlbw"
+    },
+    {
+        id: 3,
+        title: "Cologne",
+        location: "Germany",
+        googleMapsUrl: "https://goo.gl/maps/ycmnfvebffpcJCCK6",
+        startDate: "18 Jan, 2021",
+        endDate: "22 Jan, 2021",
+        description: "German Köln, fourth largest city in Germany and largest city of the Land (state) of North Rhine–Westphalia. One of the key inland ports of Europe, it is the historic, cultural, and economic capital of the Rhineland.",
+        imageUrl: "https://unsplash.com/photos/19T-waevMzE" 
+    },
+    {
+        id: 4,
+        title: "Berchtesgaden",
+        location: "Germany",
+        googleMapsUrl: "https://goo.gl/maps/CU3TFcs4RLiEM95QA",
+        startDate: "22 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "It is situated on the Berchtesgaden Stream in a deep valley surrounded on three sides by Austrian territory, just north of Berchtesgaden National Park.",
+        imageUrl: "https://unsplash.com/photos/kLT3iD4SZwI"
+    }
     
 ]
